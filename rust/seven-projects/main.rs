d3b2c58518c7c16fb438cfcee74a4e73e0451d69
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-02-12 10:03:12 -0500

    Start 7

---
+++ rust/seven-projects/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("==================================================");
+    println!("Chapter 7: Managing Growing Projects with Packages, Crates, and Modules");
+    println!("==================================================");
+}
