7bb7fec8b806654e17840d6bccab1a29d46673d3
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-02-12 09:26:27 -0500

    Finish enums and match, and if let

--- rust/six-enums/main.rs
+++ rust/six-enums/main.rs
@@ -30,4 +30,63 @@ fn main() {
 
         println!("home = {:#?}, loopback = {:#?}", home, loopback);
     }
+
+    {
+        println!("==================================================");
+        println!("Chapter 6: match control flow");
+        println!("==================================================");
+        #[derive(Debug)]
+        enum UsState {
+            Alabama,
+            Alaska,
+            //--snip--
+        }
+        enum Coin {
+            Penny,
+            Nickel,
+            Dime,
+            Quarter(UsState),
+        }
+
+        let alabama_quarter = Coin::Quarter(UsState::Alabama);
+        match alabama_quarter {
+            Coin::Penny => {
+                println!("A lucky penny!");
+                println!("1")
+            },
+            Coin::Nickel => println!("5"),
+            Coin::Dime => println!("10"),
+            Coin::Quarter(state) => println!("Your have a quarter from {:?}", state),
+        }
+    }
+
+    {
+        println!("==================================================");
+        println!("Chapter 6: if let");
+        println!("==================================================");
+
+        let config_max = Some(3u8);
+        match config_max {
+            Some(max) => println!("the maximum is configured to be {}", max),
+            _ => (),
+        }
+
+        let config_max2 = Some(10u8);
+        match config_max2 {
+            Some(max) => println!("the maximum is configured to be {}", max),
+            _ => (),
+        }
+
+        let config_max3 = Some(20u8);
+        if let Some(max) = config_max3 {
+            println!("The maximum is configured to be {}", max)
+        }
+
+        let config_max4: Option<u8> = None;
+        if let Some(max) = config_max4 {
+            println!("The maximum is configured to be {}", max);
+        } else {
+            println!("literally anything else");
+        }
+    }
 }
