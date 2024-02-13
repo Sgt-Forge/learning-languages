f85fd3a1931e95265ee8db4771b3f85d79bc3544
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-02-13 10:04:28 -0500

    more project and module system work

--- rust/seven-projects/src/main.rs
+++ rust/seven-projects/src/main.rs
@@ -1,5 +1,12 @@
+use crate::garden::vegetables::Asparagus;
+
+pub mod garden;
+
 fn main() {
     println!("==================================================");
     println!("Chapter 7: Managing Growing Projects with Packages, Crates, and Modules");
     println!("==================================================");
+
+    let plant = Asparagus {};
+    println!("I'm growing {:?}", plant);
 }
