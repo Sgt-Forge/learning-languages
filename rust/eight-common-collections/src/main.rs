0271123040a1dcdc740b0421d91da71a5bb3a083
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-02-17 13:07:39 -0500

    Finish chapter 8

--- rust/eight-common-collections/src/main.rs
+++ rust/eight-common-collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::Collections::HashMap;
+
 fn print_type_of<T>(_value: &T) {
     println!("Type: {}", std::any::type_name::<T>());
 }
@@ -63,4 +65,89 @@ fn main() {
 
         v.clear();
     }
+
+    println!("==================================================");
+    println!("Chapter 8: More on String");
+    println!("==================================================");
+    {
+        let s1 = "initial contents".to_string();
+        let s2 = String::from("initial contents");
+        let s3 = String::new();
+        let hello = String::from("Здравствуйте");
+        println!("s1 = {s1}, s2 = {s2}, s3 = {s3}, hello = {hello}");
+    }
+
+    println!("==================================================");
+    println!("Chapter 8: Updating and growing string");
+    println!("==================================================");
+    {
+        let mut s1 = String::from("foo");
+        s1.push_str("bar");
+        println!("s1 = {s1}");
+    }
+    {
+        let mut s = String::from("foo");
+        let s2 = "bar";
+        s.push_str(s2); // Takes a string slice because we don't want to take ownership of the parameter. We want to be able to use the parameter afterwards
+        println!("s2 is {s2}");
+        let mut s3 = String::from("lo");
+        s3.push('l');
+        println!("s = {s}, s3 = {s3}");
+    }
+
+    println!("==================================================");
+    println!("Chapter 8: + operator");
+    println!("==================================================");
+    {
+        let s1 = String::from("hello, ");
+        let s2 = String::from(" world");
+        let s3 = s1 + &s2; // s1 is moved here so it is no longer valid after this line
+
+        // println!("s1 = {s1}");
+        println!("s3 = {s3}, s2 = {s2}");
+    }
+
+    {
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+        let s = s1 + "-" + &s2 + "-" + &s3; // Okay all variables to the right of s1 have to be references, becuase the order of operations moves left to right
+        println!("s = {s}");
+        println!("s2 = {s2}");
+        println!("s3 = {s3}");
+    }
+    {
+        let s1 = String::from("tic");
+        let s2 = String::from("tac");
+        let s3 = String::from("toe");
+        let s4 = format!("{s1}-{s2}-{s3}");
+        println!("s4 = {s4}");
+    }
+    println!("==================================================");
+    println!("Chapter 8: hash map");
+    println!("==================================================");
+    {
+        let map = HashMap::new();
+        map.insert("Blue", 10);
+        map.insert("Yellow", 50);
+        let team_name = String::from("Blue");
+        let score = map.get(&team_name).copied().unwrap_or(0);
+        // get's the score by passing string slice, a reference because we don't want to own the parameter
+        // get retruns an option because there is a chance that we won't find what we are looking for in the map
+        // So we get Option<&i32>
+        // copied() because we want Option<i32>
+        // Unwrap so we get the value, pass the value that we get if None is returned.
+    }
+    println!("==================================================");
+    println!("Chapter 8: iterating");
+    println!("==================================================");
+    {
+        let map = HashMap::new();
+        map.insert("Blue", 10);
+        map.insert("Yellow", 50);
+        for (key, value) in &map {
+            println!("{key} = {value}");
+        }
+    }
+    
 }
