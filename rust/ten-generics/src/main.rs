44a6b0ab78abad4b3118ff99c0f0f53fb77a2e5a
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-02-24 21:36:29 -0500

    chapter 10

--- rust/ten-generics/src/main.rs
+++ rust/ten-generics/src/main.rs
@@ -10,14 +10,70 @@ fn main() {
         println!("largest1 = {:?}", largest1);
         println!("largest2 = {:?}", largest2);
     }
+    println!("==================================================");
+    println!("Chapter 10: implementing traits on types");
+    println!("==================================================");
+    {
+        pub trait Summary {
+            fn summarize(&self) -> String;
+        }
+        pub struct NewsArticle {
+            pub headline: String,
+            pub location: String,
+            pub author: String,
+            pub content: String,
+        }
+        pub struct Tweet{
+            pub username: String,
+            pub content: String,
+            pub reply: String,
+            pub retweet: String,
+        }
+        impl Summary for NewsArticle {
+            fn summarize(&self) -> String {
+                format!("{}, by {} ({})", self.headline, self.author, self.location)
+            }
+        }
+        impl Summary for Tweet{
+            fn summarize(&self) -> String {
+                format!("{}: {}", self.username, self.content)
+            }
+        }
+        let article = NewsArticle {
+            headline: String::from("Testing Traits"),
+            location: String::from("New York, NY"),
+            author: String::from("Paul Nakonieczny"),
+            content: String::from("This is an article about testing traits on types."),
+        };
+        let tweet = Tweet {
+            username: String::from("sgt.forge"),
+            content: String::from("this is a cool tweet"),
+            reply: String::from("reply"),
+            retweet: String::from("retweet"),
+        };
+        println!("{}", article.summarize());
+        println!("{}", tweet.summarize());
+    }
+    println!("==================================================");
+    println!("Chapter 10: traits as parameters");
+    println!("==================================================");
+    {
+        pub trait Summary {
+            fn summarize(&self) -> String;
+        }
+        pub fn notify(item: &impl Summary) {
+            println!("Breaking new! {}", item.summarize());
+        }
+        
+    }
 }
 
 fn largest_copy(list: &[i32]) -> i32 {
-    let mut largest = &list[0];
+    let mut largest: i32 = list[0];
 
     for item in list {
-        if item > largest {
-            largest = item;
+        if item > &largest {
+            largest = *item;
         }
     }
 
