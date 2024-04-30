bb718f1a16b304949081ef8cab0e7a8aac943334
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2024-04-30 10:01:34 -0400

    Implement custom box type

--- rust/fifteen-smart-pointers/src/main.rs
+++ rust/fifteen-smart-pointers/src/main.rs
@@ -1,4 +1,32 @@
+use std::ops::Deref;
+
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
+
+    struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    // assert_eq!(5, x);
+    // assert_eq!(5, *y); compile time error because MyBox cannot be 
+                        // dereferenced
+    
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // compiles because we implemented deref
 }
