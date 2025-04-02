60370a053c9321eccb9cb1df8dc5505484a9f1c1
Paul Nakonieczny <nakonieczny.paul@gmail.com>
2025-04-02 09:40:46 -0400

    concurrency

--- rust/fifteen-smart-pointers/src/main.rs
+++ rust/fifteen-smart-pointers/src/main.rs
@@ -1,4 +1,6 @@
 use std::ops::Deref;
+use std::rc::Rc;
+use std::cell::Cell;
 
 fn main() {
     let b = Box::new(5);
@@ -52,4 +54,38 @@ fn main() {
     println!("Created another custom smart pointer.");
     drop(e);
     println!("Dropping the new CustomSmartPointer before the end of main.");
+
+    {
+        enum List {
+            Cons(i32, Rc<List>),
+            Nil,
+        }
+
+        let a = Rc::new(List::Cons(5, Rc::new(List::Cons(10, Rc::new(List::Nil)))));
+        println!("Count after creating a = {}", Rc::strong_count(&a));
+        let _b = List::Cons(3, Rc::clone(&a));
+        println!("Count after creating b = {}", Rc::strong_count(&a));
+        {
+            let _c = List::Cons(4, Rc::clone(&a));
+            println!("Count after creating c = {}", Rc::strong_count(&a));
+        }
+        println!("Count after c goes out of scope = {}", Rc::strong_count(&a));
+    }
+
+    {
+        fn foo(cell: &Cell<u32>) {
+            let value = cell.get();
+            cell.set(value * 2);
+        }
+
+        let cell = Cell::new(0);
+        let value = cell.get();
+        println!("the value of value: {}", value);
+        let new_value = cell.get() + 1;
+        println!("the value of new_value: {}", new_value);
+        foo(&cell);
+        println!("the value of cell after foo: {}", cell.get());
+        cell.set(new_value);
+        println!("the value of cell after cell.set(new_value): {}", cell.get());
+    }
 }
